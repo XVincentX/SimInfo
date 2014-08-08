d0d533b9a3ed9e16c2e9bfdaf7895f5da554dd7d
Vincenzo Chianese <darkfrenesy@yahoo.it>
2014-08-08 16:41:04 +0000

    Branched from $/SimInfo/WindInfo

---
+++ SimInfo_8.1/Tiles/336_336.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+
+namespace Tiles
+{
+    public partial class _336_336 : UserControl
+    {
+        public _336_336()
+        {
+            InitializeComponent();
+        }
+
+        private void StackPanel_Loaded(object sender, RoutedEventArgs e)
+        {
+
+            var panel = sender as StackPanel;
+
+#if WINDOWS_PHONE
+            var objs = panel.Children.Where(x => x.Visibility == System.Windows.Visibility.Collapsed).ToArray();
+#else
+            var objs = panel.Children.Cast<UIElement>().Where(x => x.Visibility == System.Windows.Visibility.Collapsed).ToArray();
+#endif
+
+
+            
+            var invCount = objs.Count();
+
+            for (int i = 0; i < invCount; i++)
+                panel.Children.Remove(objs.ElementAt(i));
+        }
+
+    }
+}
