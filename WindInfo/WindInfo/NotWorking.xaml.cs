e6959def9944373549e3601c431512017497dabb
Vincenzo Chianese <vincenz.chianese@yahoo.it>
2014-12-01 21:40:21 +0000

    Trello bug submission

--- WindInfo/WindInfo/NotWorking.xaml.cs
+++ WindInfo/WindInfo/NotWorking.xaml.cs
@@ -10,6 +10,9 @@ using Microsoft.Phone.Shell;
 using Microsoft.Phone.Tasks;
 using StringResources;
 using WindInfo.Code;
+using HttpClientExtras;
+using System.Net.Http;
+using Coding4Fun.Toolkit.Controls;
 
 namespace WindInfo
 {
@@ -18,14 +21,50 @@ namespace WindInfo
         public NotWorking()
         {
             InitializeComponent();
+            HttpClientExtras.WP8.PlatformAdapters.Init();
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private async void Button_Click(object sender, RoutedEventArgs e)
         {
-            if (textProblem.Text.Length > 0)
-                (new EmailComposeTask { To = "vincenz.chianese@yahoo.it", Subject = AppResources.TitleMailError, Body = textProblem.Text }).Show();
-            else
+
+            if (!(textProblem.Text.Length > 0))
                 MessageBox.Show(AppResources.MissingText);
+
+            (FindName("Send") as Control).IsEnabled = false;
+            SystemTray.SetProgressIndicator(this, new ProgressIndicator { IsVisible = true, IsIndeterminate = true, Text = "Sending..." });
+            
+            using (var webclient = new HttpClient(new OAuthProtectedResourceMessageHandler { ConsumerKey = "14a8a3b2e97716d92d21f1d66a58f6f3", ConsumerSecret = "f20f027623385fbafdb905622c78a5f18066ea00255390d3ffb61e16aa9b3dbe", AccessToken = "fd65690a317fbc6454dbbe15108160facc2b8d916889f2f88af9fd8656abe32a" }, true) { BaseAddress = new Uri("https://api.trello.com/1/") })
+            {
+                var result = await webclient.PostAsJsonAsync("cards",
+                new
+                {
+                    name = "Support request",
+                    desc = textProblem.Text.Substring(0, Math.Min(16384, textProblem.Text.Length)),
+                    due = "null",
+                    idList = "547c43fe357e27da9bf82d6c",
+                    urlSource = "null",
+                    labels = "blue"
+                });
+
+                try
+                {
+                    result.EnsureSuccessStatusCode();
+                    var msg = new MessagePrompt() { Body = AppResources.RequestSent };
+                    msg.Completed += new EventHandler<PopUpEventArgs<string, PopUpResult>>((s, a) => { NavigationService.GoBack(); });
+                    msg.Show();
+                }
+                catch (Exception)
+                {
+                    (new EmailComposeTask { To = "vincenz.chianese@yahoo.it", Subject = AppResources.TitleMailError, Body = textProblem.Text }).Show();
+                }
+                finally
+                {
+                    SystemTray.SetProgressIndicator(this, new ProgressIndicator { IsVisible = false });
+                    (FindName("Send") as Control).IsEnabled = true;
+                }
+
+            }
+
         }
 
         private void Reset_Click(object sender, RoutedEventArgs e)
